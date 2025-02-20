f5bfe53642ed0a419883e20fe256ed18b56a8dd5
joeystacey <joey.stacey6@gmail.com>
2025-02-20 10:27:59 -0700

    Created a function to mulitply
    Takes 2 numbers and multiplies them

---
+++ mulitply.py
@@ -0,0 +1,8 @@
+# Joey Stacey 2/20/25
+# Take input and mulitply it
+
+# takes 2 numbers and returns the result
+def mulitplyTwoNumbers(iNum1, iNum2) :
+    result =iNum1 * iNum2
+    return result
+
